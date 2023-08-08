97e3b85a4932b5efbfa023b9bd65954729b29690
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-08 08:36:42 -0500

    feat(Root): Initialiize passport
    middleware to Initialiize passport

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 import express from "express"; 
 import morgan from "morgan";
 import auth from "./router/auth.js";
+import passport from "passport";
 
 const app = express(); 
-
-
 /**
  * ! middlewares
  */
-
-
 app.use(express.json());
 app.use(express.text()); 
 app.use(morgan("dev"))
-
+app.use(passport.initialize()); 
 
 /**
  *  * Routes 
