9a5b322e78ff5f920df5e8f408e71a8f0af6b214
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-08 08:39:44 -0500

    feat(Root): the word Return
    for return the promise

--- src/libs/token.js
+++ src/libs/token.js
@@ -4,8 +4,8 @@ import globalProperties from "../env/env.js";
 const generateToken = () => {
     return new Promise((resolve, reject) => {
         let numberRand = Math.floor(Math.random() * 10)
-        jwt.sign({id: numberRand}, globalProperties.KEY, {expiresIn: "5m", algorithm: "HS256"}, (err, token) => {
-            err ? reject(err) : resolve(token)
+        jwt.sign({id: numberRand}, globalProperties.KEY, {expiresIn: "5m"}, (err, token) => {
+            return err ? reject(err) : resolve(token)
         })  
     })
 } 
